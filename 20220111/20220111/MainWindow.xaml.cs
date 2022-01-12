42393164c36257fd3213e8bf135035e93baaccaa
waten <gogowaten@yahoo.co.jp>
2022-01-12 13:36:25 +0900

    202201121336

--- 20220111/20220111/MainWindow.xaml.cs
+++ 20220111/20220111/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace _20220111
 {
@@ -21,25 +22,87 @@ namespace _20220111
     public partial class MainWindow : Window
     {
         TThumb MyTThumb;
+        TThumb MyGTThumb;
         public MainWindow()
         {
             InitializeComponent();
 
-            TextBlock tb = new() { Text = "test", FontSize = 30, Background = Brushes.MediumAquamarine };
-            tb.RenderTransform = new ScaleTransform(2, 2);
-            MyTThumb = new(tb);
-            MyCanvas.Children.Add(MyTThumb);
+            //TextBlock tb = new() { Text = "test", FontSize = 30, Background = Brushes.MediumAquamarine };
+            //tb.RenderTransform = new ScaleTransform(2, 2);
+            //MyTThumb = new(tb);
+            //MyCanvas.Children.Add(MyTThumb);
+            //TTT tTT = new();
+            ////MyTThumb.DragDelta += MyTThumb_DragDelta;
+
+            List<TThumb> thumbs = new();
+            for (int i = 0; i < 3; i++)
+            {
+                TThumb t = new TThumb(MakeTextBlock($"test{i}"));
+                t.MyData.X = i * 30; t.MyData.Y = i * 30;
+                thumbs.Add(t);
+            }
+            MyGTThumb = new TThumb(thumbs);
+            //MyGTThumb.DragDelta += MyTThumb_DragDelta;
+            MyCanvas.Children.Add(MyGTThumb);
+
+        }
+
+        private TextBlock MakeTextBlock(string text)
+        {
+            TextBlock tb = new()
+            {
+                Text = text,
+                FontSize = 30,
+                Background = Brushes.MediumAquamarine,
+                Foreground = Brushes.White
+            };
+            return tb;
+        }
+
+        private void MyTThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            TThumb t = sender as TThumb;
+            t.MyData.X += e.HorizontalChange;
+            t.MyData.Y += e.VerticalChange;
         }
 
         private void ButtonTest_Click(object sender, RoutedEventArgs e)
         {
-            var neko = MyTThumb.ActualWidth;
-            var inu = MyTThumb.Width;
-            var element = MyTThumb.MyContentElement;
-            var ew = element.Width;
-            var eaw = element.ActualWidth;
-            MyTThumb.GetSize();
-           var mcaw= MyCanvas.ActualWidth;
+            //var neko = MyTThumb.ActualWidth;
+            //var inu = MyTThumb.Width;
+            //var element = MyTThumb.MyContentElement;
+            //var ew = element.Width;
+            //var eaw = element.ActualWidth;
+            //var mcaw = MyCanvas.ActualWidth;
+
+            var aw = MyGTThumb.ActualWidth;
+            var w = MyGTThumb.Width;
+            var left = MyGTThumb.MyData.X;
+            var top = MyGTThumb.MyData.Y;
+            var data = MyGTThumb.MyData;
+        }
+
+        private void ButtonTest2_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyGTThumb.IsEditInsideGroup)
+            {
+                MyGTThumb.IsEditInsideGroup = false;
+                //MyGTThumb.DragDelta += MyTThumb_DragDelta;
+                //foreach (TThumb item in MyGTThumb.Children)
+                //{
+                //    item.DragDelta -= MyTThumb_DragDelta;
+                //}
+            }
+            else
+            {
+                MyGTThumb.IsEditInsideGroup = true;
+                //MyGTThumb.DragDelta -= MyTThumb_DragDelta;
+                //foreach (TThumb item in MyGTThumb.Children)
+                //{
+                //    item.DragDelta += MyTThumb_DragDelta;
+                //}
+            }
+            
         }
     }
 }
--- 20220111/20220111/TThumb.cs
+++ 20220111/20220111/TThumb.cs
@@ -20,7 +20,7 @@ using System.Globalization;
 
 namespace _20220111
 {
-    internal class BaseThumb : Thumb
+    public abstract class BaseThumb : Thumb
     {
         protected Canvas RootCanvas;
         private readonly string ROOT_CANVAS_NAME = "root";
@@ -50,66 +50,182 @@ namespace _20220111
             //this.Height = element.ActualHeight;
         }
     }
-    class TThumb : BaseThumb
+    public class TThumb : BaseThumb
     {
         public Data MyData = new();
         public FrameworkElement MyContentElement { get; private set; }
+        public bool IsEditInsideGroup//グループ内での移動
+        {
+            get => isEditInsideGroup;
+            set
+            {
+                if (IsGroup == false) { return; }
+                isEditInsideGroup = value;
+                if (value)
+                {
+                    this.DragDelta -= TThumb_DragDelta;
+                    foreach (var item in Children)
+                    {
+                        item.DragDelta += item.TThumb_DragDelta;
+                        item.DragStarted += item.TThumb_DragStarted;
+                        item.DragCompleted += item.TThumb_DragCompleted;
+                    }
+                }
+                else
+                {
+                    this.DragDelta += TThumb_DragDelta;
+                    foreach (var item in Children)
+                    {
+                        item.DragDelta -= item.TThumb_DragDelta;
+                        item.DragStarted -= item.TThumb_DragStarted;
+                        item.DragCompleted -= item.TThumb_DragCompleted;
+                    }
+                }
+            }
+        }
+
         public List<TThumb> Children;
+        private bool isEditInsideGroup;//グループ内部の編集中、移動とかに使う、グループじゃなければtrueにしたくないけど…
+        public bool IsGroup;//グループ判定、childrenが0なら必ずfalse
+        public bool IsMovable;//ドラッグ移動可能
+        private Rect DragTempRect = new();
+        public TThumb ParentThumb;//グループ時の親
 
         public TThumb()
         {
             DataContext = MyData;
-            this.SetBinding(Thumb.WidthProperty, new Binding(nameof(MyData.Width)));
-            this.SetBinding(Thumb.HeightProperty, new Binding(nameof(MyData.Height)));
+            //this.SetBinding(Thumb.WidthProperty, new Binding(nameof(MyData.Width)));
+            MySetTwoWayModeBinding(Thumb.WidthProperty, nameof(MyData.Width));
+            MySetTwoWayModeBinding(Thumb.HeightProperty, nameof(MyData.Height));
+            MySetTwoWayModeBinding(Canvas.LeftProperty, nameof(MyData.X));
+            MySetTwoWayModeBinding(Canvas.TopProperty, nameof(MyData.Y));
+
+            DragDelta += TThumb_DragDelta;
+        }
+        private void MySetTwoWayModeBinding(DependencyProperty property, string path)
+        {
+            Binding b = new(path);
+            b.Mode = BindingMode.TwoWay;
+            this.SetBinding(property, b);
         }
 
+        private void TThumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            if (ParentThumb == null) { return; }
+            if (ParentThumb.IsGroup == false || ParentThumb.IsEditInsideGroup == false) { return; }
+            DragTempRect.Union(new Rect(MyData.X, MyData.Y, Width, Height));
+            //Parentの座標変更
+            ParentThumb.MyData.X += DragTempRect.X;
+            ParentThumb.MyData.Y += DragTempRect.Y;
+            ParentThumb.MyData.Width = DragTempRect.Width;
+            ParentThumb.MyData.Height = DragTempRect.Height;
+            //Childrenの座標変更
+            foreach (TThumb item in ParentThumb.Children)
+            {
+                item.MyData.X -= DragTempRect.X;
+                item.MyData.Y -= DragTempRect.Y;
+            }
+
+        }
+
+        private void TThumb_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            if (ParentThumb == null) { return; }
+            if (ParentThumb.IsGroup == false || ParentThumb.IsEditInsideGroup == false) { return; }
+
+            TThumb t = ParentThumb.Children[0];
+            if (t == this) { t = ParentThumb.Children[1]; }
+            DragTempRect.X = t.MyData.X;
+            DragTempRect.Y = t.MyData.Y;
+            DragTempRect.Width = t.Width;
+            DragTempRect.Height = t.Height;
+            foreach (TThumb item in ParentThumb.Children)
+            {
+                if (item != this)
+                {
+                    DragTempRect.Union(new Rect(item.MyData.X, item.MyData.Y, item.Width, item.Height));
+                }
+            }
+        }
+
+        private void TThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            MyData.X += e.HorizontalChange;
+            MyData.Y += e.VerticalChange;
+        }
+
+        private void MyContentElement_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            MyData.Width = MyContentElement.ActualWidth;
+            MyData.Height = MyContentElement.ActualHeight;
+        }
 
         public TThumb(FrameworkElement element) : this()
         {
-
-            AddElement(element);//この時点ではサイズが取得できないのでLoadedイベントで
+            AddElement(element);
             MyContentElement = element;
-            Loaded += (a, b) =>
-            {
-                MyData.Width = MyContentElement.ActualWidth;
-                MyData.Height = MyContentElement.ActualHeight;
-            };
+            MyContentElement.SizeChanged += MyContentElement_SizeChanged;
+
         }
         public TThumb(List<TThumb> thumbs) : this()
         {
+            IsGroup = true;
             Children = new();
             for (int i = 0; i < thumbs.Count; i++)
             {
                 GroupCanvas.Children.Add(thumbs[i]);
                 Children.Add(thumbs[i]);
+                thumbs[i].ParentThumb = this;
             }
             Loaded += (a, b) =>
             {
                 Rect r = new();
-                foreach (var item in Children)
+                foreach (TThumb item in Children)
                 {
                     Data data = item.MyData;
                     r.Union(new Rect(data.X, data.Y, data.Width, data.Height));
+                    item.DragDelta -= item.TThumb_DragDelta;
                 }
                 MyData.X = r.X; MyData.Y = r.Y;
                 MyData.Width = r.Width; MyData.Height = r.Height;
             };
         }
 
-        public void GetSize()
+        //public void GetSize()
+        //{
+        //    var aw = MyContentElement.ActualWidth;
+        //    var ah = MyContentElement.ActualHeight;
+        //    Transform rt = MyContentElement.RenderTransform;
+        //    Rect tfb = rt.TransformBounds(new Rect(0, 0, aw, ah));
+        //    var md = MyData;
+        //    var mdw = MyData.Width;
+        //    var mcew = MyContentElement.Width;
+        //    var tw = this.Width;
+        //}
+        public override string ToString()
         {
-            var aw = MyContentElement.ActualWidth;
-            var ah = MyContentElement.ActualHeight;
-            Transform rt = MyContentElement.RenderTransform;
-            Rect tfb = rt.TransformBounds(new Rect(0, 0, aw, ah));
-            var md = MyData;
-            var mdw = MyData.Width;
-            var mcew = MyContentElement.Width;
-            var tw = this.Width;
+            //return base.ToString();
+            return $"x,y=({MyData.X}, {MyData.Y}) w,h=({MyData.Width}, {MyData.Height})";
         }
     }
+
+    public class TTT : TThumb
+    {
+        void GetSize()
+        {
+
+        }
+
+    }
+
+
+
+
+
     public class Data : System.ComponentModel.INotifyPropertyChanged
     {
+        public List<Data> ChildrenDatas = new();
+
         private double x;
         private double y;
         private int z;
