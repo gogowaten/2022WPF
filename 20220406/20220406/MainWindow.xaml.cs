ff1f4b2da2ef6727f11970e953e07080dae3f793
waten <gogowaten@yahoo.co.jp>
2022-04-08 19:28:48 +0900

    202204081928

--- 20220406/20220406/MainWindow.xaml.cs
+++ 20220406/20220406/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Controls.Primitives;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace _20220406
 {
@@ -23,6 +25,7 @@ namespace _20220406
     {
         private CThumb MainCanvas;
         private CThumb G1;
+        private TItem MyItem1;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,16 +35,30 @@ namespace _20220406
             MainCanvas.Width = 200;
             MainCanvas.Height = 100;
             MyCanvas.Children.Add(MainCanvas);
-            AddG();
-
+            //AddG();
+            Test1();
         }
-
-
+        public void Test1()
+        {
+            TextBlock text = MakeTextBlock("test1", 20);
+            MyItem1 = new TItem();
+            MyItem1.AddContent(text);
+            MyCanvas.Children.Add(MyItem1);
+        }
+        public TextBlock MakeTextBlock(string text, double fontSize)
+        {
+            return new TextBlock() { Text = text, FontSize = fontSize, Margin = new Thickness(10), Background = Brushes.MediumAquamarine };
+        }
         private void AddG()
         {
             G1 = new CThumb(new TextBlock() { Text = "text1", Margin = new Thickness(10) });
             MainCanvas.AddElement(G1, 20, 0);
 
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MyItem1.Left++;
         }
     }
 
@@ -100,5 +117,74 @@ namespace _20220406
             RootCanvas.Children.Remove(control);
         }
 
+    }
+
+    public abstract class TThumb : Thumb, System.ComponentModel.INotifyPropertyChanged
+    {
+        private double left;
+        private double top;
+        private int zin;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public double Left { get => left; set { if (left != value) { left = value; OnPropertyChanged(); } } }
+        public double Top { get => top; set { if (top != value) { top = value; OnPropertyChanged(); } } }
+        public int ZIndex { get => zin; set { if (zin != value) { zin = value; OnPropertyChanged(); } } }
+
+        public TThumb()
+        {
+            DragDelta += TThumb_DragDelta;
+
+            Binding binding = new(nameof(Left));
+            binding.Source = this;
+            this.SetBinding(Canvas.LeftProperty, binding);
+
+            binding = new(nameof(Top));
+            binding.Source = this;
+            this.SetBinding(Canvas.TopProperty, binding);
+        }
+
+        private void TThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Left += e.HorizontalChange;
+            Top += e.VerticalChange;
+        }
+
+    }
+    public class TItem : TThumb
+    {
+        private ContentControl MyContent;
+        public TItem()
+        {
+            ControlTemplate template = new();
+            template.VisualTree = new FrameworkElementFactory(typeof(ContentControl), nameof(MyContent));
+            this.Template = template;
+            this.ApplyTemplate();
+            MyContent = (ContentControl)template.FindName(nameof(MyContent), this);
+        }
+        public void AddContent(UIElement content)
+        {
+            MyContent.Content = content;
+        }
+    }
+
+    public class TGroup : TThumb
+    {
+        private Canvas MyCanvas;
+        private System.Collections.ObjectModel.ObservableCollection<TThumb> myThumbs;
+        public TGroup()
+        {
+            ControlTemplate template = new();
+            template.VisualTree = new FrameworkElementFactory(MyCanvas.GetType(), nameof(MyCanvas));
+            this.Template = template;
+            this.ApplyTemplate();
+            MyCanvas = (Canvas)template.FindName(nameof(MyCanvas), this);
+            MyCanvas.
+        }
+
+        public ObservableCollection<TThumb> MyThumbs { get => myThumbs; set => myThumbs = value; }
     }
 }
---
+++ 20220408/20220408/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+//Canvasコントロールの子要素を動的に増減させたい
+//https://teratail.com/questions/359699
+
+
+namespace _20220408
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Item> Items { get; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            Items = new ObservableCollection<Item>
+            {
+                new PathItem { X = 50, Y = 50, Width = 50, Height = 50, Stroke = Brushes.Red, Data = "M0,25L25,50L50,25L25,0Z", },
+                new EllipseItem { X = 200, Y = 50, Width = 50, Height = 100, Fill = Brushes.Green, },
+                new ImageItem { X = 50, Y = 200, Width = 32, Height = 32, Source = new BitmapImage(new Uri("https://teratail-v2.storage.googleapis.com/uploads/avatars/u13/132786/KnkDDC5A_thumbnail_32x32.jpg")), },
+                new RectangleItem { X = 200, Y = 200, Width = 100, Height = 50, Fill = Brushes.Blue, },
+                new RichTextBoxItem { X = 400, Y = 50, Width = 300, Height = 300, Text = "RichTextBoxItem" },
+            };
+        }
+    }
+    public class Item
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class PathItem : Item
+    {
+        public Brush Stroke { get; set; }
+        public string Data { get; set; }
+    }
+
+    public class EllipseItem : Item
+    {
+        public Brush Fill { get; set; }
+    }
+
+    public class ImageItem : Item
+    {
+        public ImageSource Source { get; set; }
+    }
+
+    public class RectangleItem : Item
+    {
+        public Brush Fill { get; set; }
+    }
+
+    public class RichTextBoxItem : Item
+    {
+        public string Text { get; set; }
+    }
+}
