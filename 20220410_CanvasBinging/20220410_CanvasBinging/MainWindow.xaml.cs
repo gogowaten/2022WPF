ec40b7122c763058f232f031a34f913c346e4d2c
waten <gogowaten@yahoo.co.jp>
2022-04-10 15:20:13 +0900

    202204101520

---
+++ 20220410_CanvasBinging/20220410_CanvasBinging/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Collections.ObjectModel;
+
+namespace _20220410_CanvasBinging
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            UserControl1 userControl1 = new();
+            MyGrid.Children.Add(userControl1);
+            userControl1.Items.Add(new RectItem(10, 10, 100, 40, Brushes.Cyan));
+            userControl1.Items.Add(new EllipseItem(100, 100, 100, 40, Brushes.Cyan));
+        }
+    }
+
+}
---
+++ 20220410_CanvasBinging/20220410_CanvasBinging/UserControl1.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace _20220410_CanvasBinging
+{
+    /// <summary>
+    /// UserControl1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public ObservableCollection<Item> Items { get; } = new();
+
+        public UserControl1()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+    }
+    public class Item
+    {
+        public Item(double x, double y, double width, double height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+    public class RectItem : Item
+    {
+        public Brush Fill { get; set; }
+
+        public RectItem(double x, double y, double widht, double height, Brush fill) : base(x, y, widht, height)
+        {
+            Fill = fill;
+        }
+    }
+
+    public class EllipseItem : Item
+    {
+        public EllipseItem(double x, double y, double width, double height, Brush fill) : base(x, y, width, height)
+        {
+            Fill = fill;
+        }
+
+        public Brush Fill { get; set; }
+    }
+}
