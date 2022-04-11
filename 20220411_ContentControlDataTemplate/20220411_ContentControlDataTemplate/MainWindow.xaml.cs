ed52e6cab2a139ee1163591443f0f5fb8ae211d1
waten <gogowaten@yahoo.co.jp>
2022-04-11 22:01:20 +0900

    202204112201

---
+++ 20220411_ContentControlDataTemplate/20220411_ContentControlDataTemplate/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//ContentControlにデータをBinding
+//DataTemplateを使ってデータの種類によって表示を変える
+
+
+namespace _20220411_ContentControlDataTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UserControl1 MyUserControl1_0;
+        //private UserControl1 MyUserControl1_1;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyUserControl1_0 = new();
+            DataText text = new("てすと1", 30, 100, 10, 1);
+            MyUserControl1_0.MyData = text;
+            MyCanvas.Children.Add(MyUserControl1_0);
+
+            MyCanvas.Children.Add(new UserControl1() { MyData = new DataRect(Brushes.Cyan, 20, 10, 0, 400, 100) });
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            var neko = MyUserControl1_0.MyData;
+            MyCanvas.Children.Add(new UserControl1() { MyData = new DataText("追加TextBlock", 20, 100, 100,2) });
+            //TestControl.MyData = new DataText("追加TextBlocl", 30, 200, 2, 100, 100);//反映されない
+            //MyUserControl1_1.MyData.Y = 50;//反映されない
+        }
+    }
+}
---
+++ 20220411_ContentControlDataTemplate/20220411_ContentControlDataTemplate/UserControl1.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20220411_ContentControlDataTemplate
+{
+    /// <summary>
+    /// UserControl1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public Data MyData { get; set; }
+        public UserControl1()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+
+    public class Data
+    {
+        public Data() { }
+        public Data(double x, double y, double z, double width, double height)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Width = width;
+            Height = height;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+    public class DataText : Data
+    {      
+        public DataText(string text, double fontSize, double x, double y, double z) : base(x, y, z, double.NaN, double.NaN)
+        {
+            Text = text;
+            FontSize = fontSize;
+        }
+
+        public string Text { get; set; }
+        public double FontSize { get; set; }
+    }
+    public class DataRect : Data
+    {
+        public DataRect(Brush brush, double x, double y, double z, double width, double height) : base(x, y, z, width, height)
+        {
+            Brush = brush;
+        }
+
+        public Brush Brush { get; set; }
+    }
+}
