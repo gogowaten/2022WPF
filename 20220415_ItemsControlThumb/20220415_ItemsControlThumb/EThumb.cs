81217024f1cb0eccfb4604d46b43b71b2e55f018
waten <gogowaten@yahoo.co.jp>
2022-04-15 20:45:44 +0900

    202204152045

---
+++ 20220415_ItemsControlThumb/20220415_ItemsControlThumb/EThumb.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Windows.Controls.Primitives;
+
+
+namespace _20220415_ItemsControlThumb
+{
+    public class EThumb : Thumb
+    {
+        public EThumb(double x = 0, double y = 0)
+        {
+            Canvas.SetLeft(this, x);
+            Canvas.SetTop(this, y);
+        }
+    }
+    public class ItemThumb : EThumb
+    {
+        private ContentControl MyContent;
+        public ItemThumb(double x = 0, double y = 0) : base(x, y)
+        {
+            ControlTemplate template = new();
+            template.VisualTree = new(typeof(ContentControl), nameof(MyContent));
+            this.Template = template;
+            this.ApplyTemplate();
+
+            MyContent = (ContentControl)template.FindName(nameof(MyContent), this);
+        }
+        public ItemThumb(UIElement element, double x = 0, double y = 0) : this(x, y)
+        {
+            MyContent.Content = element;
+        }
+
+    }
+    public class GroupThumb : EThumb
+    {
+        public ObservableCollection<ItemThumb> Items { get; set; }
+        public GroupThumb() { }
+        public GroupThumb(double x = 0, double y = 0) : base(x, y)
+        {
+            this.DataContext = this;
+            Items = new();
+
+            FrameworkElementFactory canvasF = new(typeof(Canvas));
+            canvasF.SetValue(BackgroundProperty, Brushes.Beige);
+
+            FrameworkElementFactory itemsControlF = new(typeof(ItemsControl));
+            itemsControlF.SetValue(ItemsControl.ItemsSourceProperty, new Binding(nameof(Items)));
+            itemsControlF.SetValue(ItemsControl.ItemsPanelProperty, new ItemsPanelTemplate(canvasF));
+
+            ControlTemplate template = new();
+            template.VisualTree = itemsControlF;
+            this.Template = template;
+            this.ApplyTemplate();
+
+
+        }
+    }
+}
---
+++ 20220415_ItemsControlThumb/20220415_ItemsControlThumb/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+namespace _20220415_ItemsControlThumb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyCanvas.Children.Add(new ItemThumb(10, 10));
+            MyCanvas.Children.Add(new ItemThumb(new TextBlock { Text = "textblock", Background = Brushes.Gold }, 100, 10));
+            Rectangle rect = new() { Width = 100, Height = 100, Fill = Brushes.MediumAquamarine };
+            ItemThumb item = new(rect, 10, 100);
+            item.DragDelta += Element_DragDelta;
+            MyCanvas.Children.Add(item);
+
+
+            GroupThumb group = new(100, 100);
+            group.Items.Add(new(new TextBlock() { Text = "item1", Background = Brushes.Magenta }, 0, 0));
+            group.Items.Add(new(new TextBlock() { Text = "item2", Background = Brushes.Orange }, 100, 100));
+            group.DragDelta += Element_DragDelta;
+            MyCanvas.Children.Add(group);
+        }
+
+        private void Element_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            UIElement element = sender as UIElement;
+            Canvas.SetLeft(element, Canvas.GetLeft(element) + e.HorizontalChange);
+            Canvas.SetTop(element, Canvas.GetTop(element) + e.VerticalChange);
+        }
+    }
+}
