09c565a94b0b48854dba94f09a4b20f5f90fc960
waten <gogowaten@yahoo.co.jp>
2022-05-08 19:45:01 +0900

    202205081944

--- 20220508/20220508/MainWindow.xaml.cs
+++ 20220508/20220508/MainWindow.xaml.cs
@@ -12,17 +12,189 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Windows.Controls.Primitives;
+using System.Globalization;
+using System.ComponentModel;
+using System.Runtime.Serialization;
 
 namespace _20220508
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            #region T1
+            TThumb1 thumb1 = new TThumb1();
+            thumb1.Text = "serialTest";
+            thumb1.X = 100;
+            thumb1.Y = 100;
+            thumb1.Geometry = new RectangleGeometry(new Rect(0, 20, 10, 40));
+            thumb1.DataSave();
+            #endregion T1
+            T2Layer t2l = new();
+            t2l.SetEditing();
+
         }
+    }
+
+    public enum DataType
+    {
+        Layer = 0,
+        Group,
+        TextBlock,
+        Path,
+        Image,
+
+    }
+
+    #region //データも含めたThumbのシリアライズテスト
+    
+    //データも含めたThumbのシリアライズテスト
+    //シリアライズはこのままだとできないので、別クラスで行うようにしたけど
+    //シリアライズする項目が重複になるので冗長
+    //項目を増やすときは両方に各必要がある、めんどくさい
+    public class TThumb1 : Thumb, INotifyPropertyChanged
+    {
+        private double _x;
+        private double _y;
+        private string _text = "";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public TThumb1? MyGroup { get; set; }
+        public DataType DataType { get; set; }
+
+
+        public double X { get => _x; set { if (_x == value) { return; } _x = value; OnPropertyChanged(); } }
+
+        public double Y { get => _y; set { if (_y == value) { return; } _y = value; OnPropertyChanged(); } }
+
+        public string Text { get => _text; set { if (_text == value) { return; } _text = value; OnPropertyChanged(); } }
+        public Brush Brush { get; set; }
+        public Geometry Geometry { get; set; }
+        public TThumb1()
+        {
+
+        }
+        public void DataSave()
+        {
+            //シリアライズ
+            TThumb1Serial serial = new();
+            serial.Save(this, $"E:\\MyData.xml");
+        }
+    }
+
+    //シリアライズ用クラス
+    [DataContract]
+    [KnownType(typeof(RectangleGeometry)),
+        KnownType(typeof(MatrixTransform))]
+    public class TThumb1Serial
+    {
+        [DataMember]
+        public DataType DataType { get; set; }
+        [DataMember]
+        public double X { get; set; }
+        [DataMember]
+        public double Y { get; set; }
+        [DataMember]
+        public string Text { get; set; } = "";
+        [DataMember]
+        public Geometry? Geometry { get; set; }
+        [DataMember]
+        public Brush? Brush { get; set; }
+
+        public TThumb1Serial() { }
+
+
+        private void SubSave(string fileName)
+        {
+            System.Xml.XmlWriterSettings settings = new()
+            {
+                Encoding = new UTF8Encoding(false),
+                Indent = true,
+                NewLineOnAttributes = false,
+                ConformanceLevel = System.Xml.ConformanceLevel.Fragment
+            };
+            System.Xml.XmlWriter xmlWriter;
+            System.Runtime.Serialization.DataContractSerializer serializer = new(this.GetType());
+            using (xmlWriter = System.Xml.XmlWriter.Create(fileName, settings))
+            {
+                try
+                {
+                    serializer.WriteObject(xmlWriter, this);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        public void Save(TThumb1 thumb, string fileName)
+        {
+            X = thumb.X; Y = thumb.Y; Text = thumb.Text;
+            Geometry = thumb.Geometry;
+            DataType = thumb.DataType;
+            Brush = thumb.Brush;
+            SubSave(fileName);
+        }
+    }
+    #endregion //データも含めたThumbのシリアライズテスト
+
+
+    public class TThumb2 : Thumb
+    {
+        public T2Group MyParentGroup;
+        public T2Layer MyLayer;
+        public TThumb2() { }
+        
+    }
+    public class T2Item : TThumb2 { }
+    public class T2Group : TThumb2 {
+        public bool IsNowEditing { get; private set; }
+        public void SetEditing()
+        {
+            IsNowEditing = true;
+        }
+    }
+    public class T2Layer : T2Group { }
+
+    [DataContract]
+    [KnownType(typeof(RectangleGeometry)),
+        KnownType(typeof(MatrixTransform))]
+    public class Data2 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private double _x;
+        private double _y;
+        private string _text = "";
+        public TThumb1? ParentGroup { get; set; }
+
+        [DataMember]
+        public ObservableCollection<Data2>? ChildrenData { get; set; }
+        [DataMember]
+        public DataType DataType { get; set; }
+        [DataMember]
+        public double X { get => _x; set { if (_x == value) { return; } _x = value; OnPropertyChanged(); } }
+        [DataMember]
+        public double Y { get => _y; set { if (_y == value) { return; } _y = value; OnPropertyChanged(); } }
+        [DataMember]
+        public string Text { get => _text; set { if (_text == value) { return; } _text = value; OnPropertyChanged(); } }
+        [DataMember]
+        public Brush? Brush { get; set; }
+        [DataMember]
+        public Geometry? Geometry { get; set; }
     }
 }
