80d9949e1413ae2439c0fadd319a3325614991ba
waten <gogowaten@yahoo.co.jp>
2022-05-30 23:37:46 +0900

    202205302337

---
+++ 20220530/20220530/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Windows.Controls.Primitives;
+using System.Globalization;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Collections.Specialized;
+
+namespace _20220530
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int MyItemsCount = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            Left = 10; Top = 10;
+#endif
+            Item4 item1 = new(MakeTextBloclData1(0, 0, "Item1", 4));
+            MyLayer1.AddThumb(item1);
+            item1 = new(MakeTextBloclData1(50, 20, "Item2", 4));
+            MyLayer1.AddThumb(item1);
+
+            Group4 group = new();
+            MyLayer1.AddThumb(group);
+            group.MyData.X = 20;group.MyData.Y = 100;
+            item1 = new(MakeTextBloclData1(0, 0, "Item3", 4));
+            group.AddThumb(item1);
+            item1 = new(MakeTextBloclData1(50, 20, "Item4", 4));
+            group.AddThumb(item1);
+        }
+        private Data1 MakeTextBloclData1(double x, double y, string text, double padding)
+        {
+            Data1 data = new(DataType.TextBlock) { X = x, Y = y, Text = text, Padding = padding };
+            data.Background = new SolidColorBrush(
+                Color.FromArgb(
+                    255,
+                    100,
+                    (byte)(MyItemsCount * 20 + 100),
+                    (byte)(MyItemsCount * 20 + 150)));
+            MyItemsCount++;
+            return data;
+        }
+    }
+}
