0e05eab5b893085dfac97326fd270fd1bed4591a
waten <gogowaten@yahoo.co.jp>
2022-06-16 22:54:25 +0900

    202206162254

--- 20220615_TextBoxThumb/20220615_TextBoxThumb/MainWindow.xaml.cs
+++ 20220615_TextBoxThumb/20220615_TextBoxThumb/MainWindow.xaml.cs
@@ -4,6 +4,16 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Controls.Primitives;
+using System;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace _20220615_TextBoxThumb
 {
@@ -11,7 +21,7 @@ namespace _20220615_TextBoxThumb
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {   
+    {
         private TBThumb MyTBThumb;
         private TBThumb2 MyTBThumb2;
         public MainWindow()
@@ -33,6 +43,38 @@ namespace _20220615_TextBoxThumb
             MyTBThumb2 = t;
 
             MyGroupBox.DataContext = MyTBThumb2.TextBox;
+
+            //MyTBThumb2.TextBox.CaretBrush = Brushes.Red;
+            MyTBThumb2.TextBox.AcceptsReturn = true;
+            //MyTBThumb2.TextBox.HorizontalContentAlignment = HorizontalAlignment.Center;
+            MyTBThumb2.TextBox.FontStyle = FontStyles.Italic;
+            MyTBThumb2.TextBox.FontWeight = FontWeights.Bold;
+            MyTBThumb2.TextBox.Padding = new Thickness(10);
+            MyTBThumb2.TextBox.TextAlignment = TextAlignment.Center;//horizontalcontentalignmentとほぼ同じ
+            TextDecoration deco = new();
+            deco.Location = TextDecorationLocation.Strikethrough;//全4種類のライン
+            deco.Pen = new Pen(Brushes.Red, 1.0);
+            deco.PenOffset = 0;
+            deco.PenOffsetUnit = TextDecorationUnit.Pixel;//全3種類
+            deco.PenThicknessUnit = TextDecorationUnit.Pixel;//全3種類
+            TextDecorationCollection decoCollection = new();
+            decoCollection.Add(deco);
+            MyTBThumb2.TextBox.TextDecorations = decoCollection;
+            MyTBThumb2.TextBox.VerticalContentAlignment = VerticalAlignment.Top;
+            MyTBThumb2.TextBox.Height = 100;
+
+            TextEffect effect = new();
+            effect.Foreground = Brushes.Red;
+
+
+
+            //System.Windows.Media.Effects.EdgeProfile.CurvedOut;
+
+            //System.Windows.Media.Effects.DropShadowEffect dropShadow = new();
+            //TextBlock tb = new() { Text = "textblock" ,FontSize=50,Effect=dropShadow};
+            //MyGrid.Children.Add(tb);
+
+
         }
         private TBThumb2 MakeTBThumb2(string text)
         {
@@ -42,7 +84,28 @@ namespace _20220615_TextBoxThumb
             return t;
         }
 
-      
+        /// <summary>
+        /// フォント一覧のComboBoxを作成
+        /// </summary>
+        /// <returns></returns>
+        private static ComboBox MakeFontComboBox()
+        {
+            //DataTemplateの設定、Textblockにフォント名表示
+            FrameworkElementFactory textblockF = new(typeof(TextBlock));
+            textblockF.SetBinding(TextBlock.TextProperty,new Binding("Key"));
+            //↓フォント名表示にそのフォントを使う、リスト表示に時間がかかる
+            //textblockF.SetBinding(TextBlock.FontFamilyProperty,new Binding("Value"));
+            DataTemplate dataTemplate = new();
+            dataTemplate.VisualTree = textblockF;
+
+            ComboBox comboBox = new()
+            {
+                ItemsSource = MakeFontFamiliesDictionary(),
+                SelectedValuePath = "Value",
+                ItemTemplate = dataTemplate
+            };
+            return comboBox;            
+        }
 
         private static Dictionary<string, Brush> MakeBrushesDictionary()
         {
@@ -66,6 +129,13 @@ namespace _20220615_TextBoxThumb
             Dictionary<string, Brush> dict = MakeBrushesDictionary();
             MyComboBoxFore.ItemsSource = dict;
             MyComboBoxBack.ItemsSource = dict;
+            //フォント名一覧ComboBox
+            ComboBox box = MakeFontComboBox();
+            Binding b = new();
+            b.Path = new PropertyPath(TextBox.FontFamilyProperty);
+            box.SetBinding(ComboBox.SelectedValueProperty, b);
+            MyStackPanel.Children.Add(box);
+
         }
 
         private void MyTThumb2_DragDelta(object sender, DragDeltaEventArgs e)
@@ -75,6 +145,49 @@ namespace _20220615_TextBoxThumb
             Canvas.SetTop(elem, Canvas.GetTop(elem) + e.VerticalChange);
         }
 
+        /// <summary>
+        /// SystemFontFamiliesから日本語フォント名で並べ替えたフォント一覧を返す、1ファイルに別名のフォントがある場合も取得
+        /// </summary>
+        /// <returns></returns>
+        private static SortedDictionary<string, FontFamily> MakeFontFamiliesDictionary()
+        {
+            //今のPCで使っている言語(日本語)のCulture取得
+            //var language =
+            // System.Windows.Markup.XmlLanguage.GetLanguage(
+            // CultureInfo.CurrentCulture.IetfLanguageTag);
+            System.Globalization.CultureInfo culture = System.Globalization.CultureInfo.CurrentCulture;//日本
+            System.Globalization.CultureInfo cultureUS = new("en-US");//英語？米国？
+
+            List<string> uName = new();//フォント名の重複判定に使う
+            Dictionary<string, FontFamily> tempDictionary = new();
+            foreach (var item in Fonts.SystemFontFamilies)
+            {
+                var typefaces = item.GetTypefaces();
+                foreach (var typeface in typefaces)
+                {
+                    _ = typeface.TryGetGlyphTypeface(out GlyphTypeface gType);
+                    if (gType != null)
+                    {
+                        //フォント名取得はFamilyNamesではなく、Win32FamilyNamesを使う
+                        //FamilyNamesだと違うフォントなのに同じフォント名で取得されるものがあるので
+                        //Win32FamilyNamesを使う
+                        //日本語名がなければ英語名
+                        string fontName = gType.Win32FamilyNames[culture] ?? gType.Win32FamilyNames[cultureUS];
+                        //string fontName = gType.FamilyNames[culture] ?? gType.FamilyNames[cultureUS];
+
+                        //フォント名で重複判定
+                        var uri = gType.FontUri;
+                        if (uName.Contains(fontName) == false)
+                        {
+                            uName.Add(fontName);
+                            tempDictionary.Add(fontName, new(uri, fontName));
+                        }
+                    }
+                }
+            }
+            SortedDictionary<string, FontFamily> fontDictionary = new(tempDictionary);
+            return fontDictionary;
+        }
     }
 
     //TemplateをTextBoxにしたThumb、マウスドラッグ移動できない
@@ -156,6 +269,7 @@ namespace _20220615_TextBoxThumb
             template.VisualTree = underF;
             return template;
         }
+
     }
 
 
