1020239a7621028b529936b60f3386b59178c933
waten <gogowaten@yahoo.co.jp>
2022-06-20 12:35:40 +0900

    202206201235

---
+++ 20220620/20220620/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20220620
+{
+  
+
+    public class ReGroupData
+    {
+        public static List<TTextBox> TTextBoxList = new();
+        public ReGroupData(List<TTextBox> textBoxes)
+        {
+            TTextBoxList = textBoxes;
+            
+            foreach (var item in textBoxes)
+            {
+                item.ReGroupData = this;
+            }
+        }
+        internal void RemoveItem(TTextBox textBox)
+        {
+            TTextBoxList.Remove(textBox);
+        }
+        
+    }
+    public class TTextBox : TextBox
+    {
+        public ReGroupData? ReGroupData;
+        public TTextBox(string text)        {
+            Text = text;            
+        }
+        public void RemoveItem()
+        {
+            this.ReGroupData?.RemoveItem(this);
+        }
+    }
+}
---
+++ 20220620/20220620/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20220620
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TTextBox tt0 = new("tt000");
+            TTextBox tt1 = new("tt111");
+            TTextBox tt2 = new("tt222");
+
+            ReGroupData share = new(new List<TTextBox>() { tt0, tt1, tt2 });
+            var neko = tt0.ReGroupData;
+            var inu = tt1.ReGroupData;
+
+            tt1.RemoveItem();
+            var uma = tt2.ReGroupData;
+        }
+    }
+}
