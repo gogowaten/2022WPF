c9d71b96b133644f594bdde83e377f6150999caf
waten <gogowaten@yahoo.co.jp>
2022-06-28 14:04:11 +0900

    202206281404

--- 20220628/20220628/MainWindow.xaml.cs
+++ 20220628/20220628/MainWindow.xaml.cs
@@ -31,26 +31,11 @@ namespace _20220628
     public class Data : INotifyPropertyChanged
     {
 
-
+        private int _y;
+        public int Y { get => _y; set { if (_y == value) { return; } _y = value; OnPropertyChanged(); } }
 
         private int _x;
-        public int X
-        {
-            get => _x;
-            set
-            {
-                if (value == _x) { return; }
-                _x = value; OnPropertyChanged();
-            }
-        }
-        private int aaa;
-
-        public int fff
-        {
-            get { return aaa; }
-            set { aaa = value; }
-        }
-
+        public int X { get => _x; set { if (_x == value) { return; } _x = value; OnPropertyChanged(); } }
 
 
 
