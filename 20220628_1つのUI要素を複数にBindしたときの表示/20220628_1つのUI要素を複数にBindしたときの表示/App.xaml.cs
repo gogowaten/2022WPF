f2e1c53a8c44bc90b263e382d3a5f6f3a3b29439
waten <gogowaten@yahoo.co.jp>
2022-06-28 22:43:16 +0900

    202206282243

---
+++ 20220628_1つのUI要素を複数にBindしたときの表示/20220628_1つのUI要素を複数にBindしたときの表示/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _20220628_1つのUI要素を複数にBindしたときの表示
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
