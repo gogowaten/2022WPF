ca75350fa8146b8567d27e5d8c28bba58c6001e8
waten <gogowaten@yahoo.co.jp>
2022-09-18 22:21:28 +0900

    202209182221

---
+++ _20220918_バインドでDataContextが見つかりません/_20220918_バインドでDataContextが見つかりません/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace _20220918_バインドでDataContextが見つかりません
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Data
+    {
+        public string TextAAA { get; set; }
+        public string TextBBB { get; set; } = "確認用BBB";
+        public string TextCCC { get; set; }
+
+        public Data() { TextCCC = "確認用CCC"; }
+        public Data(string textAAA, string textBBB,string textCCC)
+        {
+            TextAAA = textAAA;
+            TextBBB = textBBB;
+            TextCCC = textCCC;
+        }
+    }
+}
