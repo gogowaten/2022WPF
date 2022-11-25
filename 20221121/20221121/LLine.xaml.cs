8b5b3d39212fb799a3e5ca597a3f2f1ac70e2a08
waten <gogowaten@yahoo.co.jp>
2022-11-25 22:19:21 +0900

    202211252219

--- 20221121/20221121/LLine.xaml.cs
+++ 20221121/20221121/LLine.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
@@ -97,7 +98,7 @@ namespace _20221121
     public class PointConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {   
+        {
             var locate = values[0] as double?;
             var size = values[1] as double?;
 
@@ -111,4 +112,100 @@ namespace _20221121
         }
     }
 
+
+
+    //    WPF Arrow and Custom Shapes - CodeProject
+    //https://www.codeproject.com/Articles/23116/WPF-Arrow-and-Custom-Shapes
+
+    public class Arrow : Shape
+    {
+        #region dp
+        public double X1
+        {
+            get { return (double)GetValue(X1Property); }
+            set { SetValue(X1Property, value); }
+        }
+        public static readonly DependencyProperty X1Property =
+            DependencyProperty.Register(nameof(X1), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+        //affectsを付けるとデザイン画面で数値変更したときに即表示が更新されるようになる
+        //Renderは表示更新
+        //Measureはサイズ変更
+        public double Y1
+        {
+            get { return (double)GetValue(Y1Property); }
+            set { SetValue(Y1Property, value); }
+        }
+        public static readonly DependencyProperty Y1Property =
+            DependencyProperty.Register(nameof(Y1), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double X2
+        {
+            get { return (double)GetValue(X2Property); }
+            set { SetValue(X2Property, value); }
+        }
+        public static readonly DependencyProperty X2Property =
+            DependencyProperty.Register(nameof(X2), typeof(double), typeof(Arrow), new PropertyMetadata(0.0));
+
+        public double Y2
+        {
+            get { return (double)GetValue(Y2Property); }
+            set { SetValue(Y2Property, value); }
+        }
+        public static readonly DependencyProperty Y2Property =
+            DependencyProperty.Register(nameof(Y2), typeof(double), typeof(Arrow), new PropertyMetadata(0.0));
+
+        [TypeConverter(typeof(LengthConverter))]
+        public double HeadSize
+        {
+            get { return (double)GetValue(HeadSizeProperty); }
+            set { SetValue(HeadSizeProperty, value); }
+        }
+        public static readonly DependencyProperty HeadSizeProperty =
+            DependencyProperty.Register(nameof(HeadSize), typeof(double), typeof(Arrow), new PropertyMetadata(0.0));
+        #endregion
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                StreamGeometry geometry = new();
+                geometry.FillRule = FillRule.EvenOdd;
+                using (var context = geometry.Open())
+                {
+                    InternalDraw(context);
+                }
+                geometry.Freeze();
+                return geometry;
+            }
+        }
+        private void InternalDraw(StreamGeometryContext context)
+        {
+            double theta = Math.Atan2(Y1 - Y2, X1 - X2);
+            double sint = Math.Sin(theta);
+            double cost = Math.Cos(theta);
+
+            Point pt1 = new Point(X1, this.Y1);
+            Point pt2 = new Point(X2, this.Y2);
+
+            Point pt3 = new Point(
+                X2 + (HeadSize * cost - HeadSize * sint),
+                Y2 + (HeadSize * sint + HeadSize * cost));
+
+            Point pt4 = new Point(
+                X2 + (HeadSize * cost + HeadSize * sint),
+                Y2 - (HeadSize * cost - HeadSize * sint));
+
+            context.BeginFigure(pt1, true, false);
+            context.LineTo(pt2, true, true);
+            context.LineTo(pt3, true, true);
+            context.LineTo(pt2, true, true);
+            context.LineTo(pt4, true, true);
+        }
+    }
 }
