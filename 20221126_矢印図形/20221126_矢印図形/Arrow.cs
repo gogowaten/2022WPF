ba943920c6ad39b6327eb527b007927cca53dd29
waten <gogowaten@yahoo.co.jp>
2022-11-26 14:08:58 +0900

    202211261408

---
+++ 20221126_矢印図形/20221126_矢印図形/Arrow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace _20221126_矢印図形
+{
+    class Arrow : Shape
+    {
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        public double X1
+        {
+            get { return (double)GetValue(X1Property); }
+            set { SetValue(X1Property, value); }
+        }
+        public static readonly DependencyProperty X1Property =
+            DependencyProperty.Register(nameof(X1), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender));
+
+    }
+}
