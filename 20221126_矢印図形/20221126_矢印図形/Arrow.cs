5a72c798e145b2346595e2e614c987348ab05882
waten <gogowaten@yahoo.co.jp>
2022-11-26 14:44:56 +0900

    202211261444

--- 20221126_矢印図形/20221126_矢印図形/Arrow.cs
+++ 20221126_矢印図形/20221126_矢印図形/Arrow.cs
@@ -15,9 +15,41 @@ namespace _20221126_矢印図形
         {
             get
             {
-                throw new NotImplementedException();
+                StreamGeometry geometry = new();
+                geometry.FillRule = FillRule.EvenOdd;
+                using (var context = geometry.Open())
+                {
+                    InternalDraw(context);
+                }
+                geometry.Freeze();
+                return geometry;
             }
         }
+
+        private void InternalDraw(StreamGeometryContext context)
+        {
+            double theta = Math.Atan2(Y1 - Y2, X1 - X2);
+            double sint = Math.Sin(theta);
+            double cost = Math.Cos(theta);
+
+            Point pt1 = new(X1, Y1);
+            Point pt2 = new(X2, Y2);
+
+            Point pt3 = new Point(
+                X2 + (HeadSize * cost - HeadSize * sint),
+                Y2 + (HeadSize * sint + HeadSize * cost));
+
+            Point pt4 = new Point(
+                X2 + (HeadSize * cost + HeadSize * sint),
+                Y2 - (HeadSize * cost - HeadSize * sint));
+
+            context.BeginFigure(pt1, true, false);
+            context.LineTo(pt2, true, true);
+            context.LineTo(pt3, true, true);
+            context.LineTo(pt2, true, true);
+            context.LineTo(pt4, true, true);
+        }
+
 
 
         public double X1
@@ -30,6 +62,52 @@ namespace _20221126_矢印図形
                 new FrameworkPropertyMetadata(0.0,
                     FrameworkPropertyMetadataOptions.AffectsMeasure |
                     FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+        public double Y1
+        {
+            get { return (double)GetValue(Y1Property); }
+            set { SetValue(Y1Property, value); }
+        }
+        public static readonly DependencyProperty Y1Property =
+            DependencyProperty.Register(nameof(Y1), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double X2
+        {
+            get { return (double)GetValue(X2Property); }
+            set { SetValue(X2Property, value); }
+        }
+        public static readonly DependencyProperty X2Property =
+            DependencyProperty.Register(nameof(X2), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double Y2
+        {
+            get { return (double)GetValue(Y2Property); }
+            set { SetValue(Y2Property, value); }
+        }
+        public static readonly DependencyProperty Y2Property =
+            DependencyProperty.Register(nameof(Y2), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+
+        public double HeadSize
+        {
+            get { return (double)GetValue(HeadSizeProperty); }
+            set { SetValue(HeadSizeProperty, value); }
+        }
+        public static readonly DependencyProperty HeadSizeProperty =
+            DependencyProperty.Register(nameof(HeadSize), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
 
     }
 }
