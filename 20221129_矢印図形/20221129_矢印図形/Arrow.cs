db239b1ad6ad813b0194f3f5c15e8cf4c631b3ec
waten <gogowaten@yahoo.co.jp>
2022-11-29 15:38:53 +0900

    202211291538

---
+++ 20221129_矢印図形/20221129_矢印図形/Arrow.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace _20221129_矢印図形
+{
+    class Arrow : Shape
+    {
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                StreamGeometry geometry = new();
+                //geometry.FillRule = FillRule.EvenOdd;//いる？
+                geometry.FillRule = FillRule.Nonzero;//こっちのほうがいいかも
+                using (var context = geometry.Open())
+                {
+                    InternalDraw0(context);
+                    StrokeStartLineCap = PenLineCap.Flat;
+                    StrokeEndLineCap = PenLineCap.Flat;
+                    Fill = Stroke;
+                }
+                geometry.Freeze();
+                UseLayoutRounding = true;
+                return geometry;
+            }
+        }
+
+        //
+        private void InternalDraw0(StreamGeometryContext context)
+        {
+            Point p1 = new(X1, Y1);
+            //座標を0.5シフト、ぼやけ防止
+            //Point p1 = new(X1 - 0.5, Y1 - 0.5);
+            
+            //直線の角度
+            double baseRadian = Math.Atan2(Y1 - Y2, X1 - X2);
+            double bCos = Math.Cos(baseRadian);
+            double bSin = Math.Sin(baseRadian);
+
+            //指定された角度
+            double radian = AngleToRadian(Angle);
+            double rCos = Math.Cos(radian);
+
+            //
+            double arrowHeadRadian = baseRadian + radian;//矢じりの角度
+
+            //直線と矢じりの接点、これの計算方法がわからなくて時間かかった
+            double zentai = HeadSize * rCos;
+            Point pJoint = new(X2 + (bCos * zentai), Y2 + bSin * zentai);
+            //座標を0.5シフト、ぼやけ防止
+            //Point pJoint = new(X2 - 0.5 + (bCos * zentai), Y2 - 0.5 + bSin * zentai);
+
+
+            Point p2 = new(X2, Y2);
+            Point p3 = new(
+                X2 + HeadSize * Math.Cos(arrowHeadRadian),
+                Y2 + HeadSize * Math.Sin(arrowHeadRadian));
+
+            arrowHeadRadian = baseRadian - radian;//反対側の矢じりの角度
+            Point p4 = new(
+                X2 + HeadSize * Math.Cos(arrowHeadRadian),
+                Y2 + HeadSize * Math.Sin(arrowHeadRadian));
+
+            //直線描画、Strokeで描画
+            context.BeginFigure(p1, true, false);
+            context.LineTo(pJoint, true, true);
+            //矢じり描画、StrokeじゃなくてFillで描画
+            context.BeginFigure(p2, true, true);//point, isFill, isClose
+            context.LineTo(p3, false, false);//point, isStroke, isSmoothJoin
+            context.LineTo(p4, false, true);
+            context.LineTo(p2, false, true);
+        }
+     
+        private static double AngleToRadian(double angle)
+        {
+            return angle / 360 * (Math.PI * 2.0);
+        }
+
+        public double X1
+        {
+            get { return (double)GetValue(X1Property); }
+            set { SetValue(X1Property, value); }
+        }
+        public static readonly DependencyProperty X1Property =
+            DependencyProperty.Register(nameof(X1), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+        public double Y1
+        {
+            get { return (double)GetValue(Y1Property); }
+            set { SetValue(Y1Property, value); }
+        }
+        public static readonly DependencyProperty Y1Property =
+            DependencyProperty.Register(nameof(Y1), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double X2
+        {
+            get { return (double)GetValue(X2Property); }
+            set { SetValue(X2Property, value); }
+        }
+        public static readonly DependencyProperty X2Property =
+            DependencyProperty.Register(nameof(X2), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double Y2
+        {
+            get { return (double)GetValue(Y2Property); }
+            set { SetValue(Y2Property, value); }
+        }
+        public static readonly DependencyProperty Y2Property =
+            DependencyProperty.Register(nameof(Y2), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+
+        public double HeadSize
+        {
+            get { return (double)GetValue(HeadSizeProperty); }
+            set { SetValue(HeadSizeProperty, value); }
+        }
+        public static readonly DependencyProperty HeadSizeProperty =
+            DependencyProperty.Register(nameof(HeadSize), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+
+        public double Angle
+        {
+            get { return (double)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.Register(nameof(Angle), typeof(double), typeof(Arrow),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+    }
+}
---
+++ 20221129_矢印図形/20221129_矢印図形/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+//矢印先端の描画位置を正しくした
+//これは矢じり部分の描画をStrokeではなくてFillで描画するようにしたからできた
+//直線部分は前回同様Strokeで描画している
+
+//新たな問題
+//直線部分と矢じり部分の描画を別々にしたせいか接合部で僅かな隙間ができてしまう
+
+namespace _20221129_矢印図形
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
