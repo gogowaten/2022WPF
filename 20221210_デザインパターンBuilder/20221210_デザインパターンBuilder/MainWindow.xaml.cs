dad27617e1ea7fdf9c8a62db2c94b203de0af49c
waten <gogowaten@yahoo.co.jp>
2022-12-11 08:12:49 +0900

    202212110812

---
+++ 20221210_デザインパターンBuilder/20221210_デザインパターンBuilder/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Builder
+//https://refactoring.guru/ja/design-patterns/builder
+
+namespace _20221210_デザインパターンBuilder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Director
+    {
+        public Director()
+        {
+
+        }
+
+    }
+    public interface Builder
+    {
+        void Reset();
+        void SetFillBrush(Brush fillBrush);
+        void GetProduct();
+    }
+    public class RectangleBuilder : Builder
+    {
+        private MyRectangle _rectangle = new();
+        public RectangleBuilder()
+        {
+            Reset();
+        }
+
+        public void GetProduct()
+        {
+            
+        }
+
+        public void Reset()
+        {
+            _rectangle = new MyRectangle();
+        }
+
+        public void SetFillBrush(Brush fillBrush)
+        {
+            _rectangle.FillBrush = fillBrush;
+        }
+    }
+    public class EllipseBuilder : Builder
+    {
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetFillBrush(Brush fillBrush)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MyRectangle
+    {
+        public Brush? FillBrush { get; set; }
+        public double LineWidth { get; set; }
+        public MyRectangle() { }
+    }
+    public class MyEllipse
+    {
+        public Brush? FillBrush { get; set; }
+        public double LineWidth { get; set; }
+        public MyEllipse() { }
+    }
+}
