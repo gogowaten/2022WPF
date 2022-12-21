56498edad25cb7108863009e7de7dbaf4a783dab
waten <gogowaten@yahoo.co.jp>
2022-12-21 10:54:06 +0900

    202212211054

---
+++ 20221221_Gridで列幅行幅を共有SharedSizeGroup/20221221_Gridで列幅行幅を共有SharedSizeGroup/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _20221221_Gridで列幅行幅を共有SharedSizeGroup
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
