cbd44aeb522d7d6ea54910a8b149658fe8d1ad3c
waten <gogowaten@yahoo.co.jp>
2022-12-22 17:33:06 +0900

    20221222_Blog

--- 20221222_ExCanvas/20221222_ExCanvas/MainWindow.xaml.cs
+++ 20221222_ExCanvas/20221222_ExCanvas/MainWindow.xaml.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+//ブログ記事
+//WPF、自動サイズ調整するCanvas、子要素のサイズや位置の変更で更新 - 午後わてんのブログ
+//https://gogowaten.hatenablog.com/entry/2022/12/22/163812
 
 namespace _20221222_ExCanvas
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.Left = 200; this.Top = 200;
         }
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
@@ -45,48 +36,49 @@ namespace _20221222_ExCanvas
         }
 
     }
+    //c# - WPF: Sizing Canvas to contain its children - Stack Overflow
+    //    https://stackoverflow.com/questions/55101074/wpf-sizing-canvas-to-contain-its-children
 
+    //子要素をレンガのようにタイル状に敷き詰める | Do Design Space
+    //    https://sakapon.wordpress.com/2010/11/18/bricktile/
+
+    //レイアウト - WPF .NET Framework | Microsoft Learn
+    //    https://learn.microsoft.com/ja-jp/dotnet/desktop/wpf/advanced/layout?view=netframeworkdesktop-4.8
+
+    /// <summary>
+    /// オートサイズのCanvas、子要素のサイズや位置の変更時にサイズ更新
+    /// </summary>
     public class ExCanvas : Canvas
     {
-
-        //protected override Size MeasureOverride(Size constraint)
-        //{
-        //    if (double.IsNaN(Width) && double.IsNaN(Height))
-        //    {
-        //        base.MeasureOverride(constraint);//これを実行してから計算
-        //        Size size = new();
-        //        foreach (var item in Children.OfType<FrameworkElement>())
-        //        {
-        //            double x = GetLeft(item) + item.DesiredSize.Width;//ActualWidthは更新されていない
-        //            double y = GetTop(item) + item.DesiredSize.Height;
-        //            if (size.Width < x) size.Width = x;
-        //            if (size.Height < y) size.Height = y;
-        //        }
-        //        return size;
-        //    }
-        //    else { return base.MeasureOverride(constraint); }
-        //}
-
-        protected override Size MeasureOverride(Size constraint)
-        {
-            return base.MeasureOverride(constraint);
-        }
         protected override Size ArrangeOverride(Size arrangeSize)
         {
-            base.ArrangeOverride(arrangeSize);
-            Size size = new();
-            foreach (var item in Children.OfType<FrameworkElement>())
+            if (double.IsNaN(Width) && double.IsNaN(Height))
             {
-                double x = GetLeft(item) + item.DesiredSize.Width;//ActualWidthは更新されていない
-                double y = GetTop(item) + item.DesiredSize.Height;
-                if (size.Width < x) size.Width = x;
-                if (size.Height < y) size.Height = y;
+                base.ArrangeOverride(arrangeSize);
+                Size size = new();
+                foreach (var item in Children.OfType<FrameworkElement>())
+                {
+                    double x = GetLeft(item) + item.ActualWidth;
+                    double y = GetTop(item) + item.ActualHeight;
+                    if (size.Width < x) size.Width = x;
+                    if (size.Height < y) size.Height = y;
+                }
+                return size;
             }
-            return size;
-            //return base.ArrangeOverride(arrangeSize);
-
+            else
+            {
+                return base.ArrangeOverride(arrangeSize);
+            }
         }
-
+        //protected override Size MeasureOverride(Size constraint)
+        //{
+        //    foreach (var item in Children.OfType<FrameworkElement>())
+        //    {
+        //        var neko = item.DesiredSize.Width;
+        //        var inu = item.ActualWidth;
+        //    }
+        //    return base.MeasureOverride(constraint);
+        //}
     }
 
 
