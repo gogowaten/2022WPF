9cd4928c975025aafb78ad2c752e75256ddfb843
waten <gogowaten@yahoo.co.jp>
2022-12-23 17:35:10 +0900

    202212231735

--- 20221223_ExCanvas/20221223_ExCanvas/MainWindow.xaml.cs
+++ 20221223_ExCanvas/20221223_ExCanvas/MainWindow.xaml.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
+
+//exCanvasの位置は固定でThumb移動で一番上のExCanvasまでサイズ更新
+//DragCompletedでMeasureするようにした
 namespace _20221223_ExCanvas
 {
     /// <summary>
@@ -33,39 +25,12 @@ namespace _20221223_ExCanvas
                 Canvas.SetTop(t, Canvas.GetTop(t) + e.VerticalChange);
             }
         }
-        private void Thumb_DragComplated(object sender, DragCompletedEventArgs e)
+        private void Thumb_DragCompleted(object sender, DragCompletedEventArgs e)
         {
             if (sender is Thumb thumb && thumb.Parent is ExCanvas canvas)
             {
                 Size size = new();
-                double xMin = double.MaxValue, yMin = double.MaxValue;
-                foreach (var item in canvas.Children.OfType<FrameworkElement>())
-                {
-                    double x = Canvas.GetLeft(item);
-                    double y = Canvas.GetTop(item);
-                    double w = x + item.ActualWidth;
-                    double h = y + item.ActualHeight;
-                    if (xMin > x) xMin = x;
-                    if (yMin > y) yMin = y;
-                    if (size.Width < w) size.Width = w;
-                    if (size.Height < h) size.Height = h;
-                }
-                size.Width -= xMin; size.Height -= yMin;
-                double left = Canvas.GetLeft(canvas);
-                double top = Canvas.GetTop(canvas);
-                if (left < 0) { left = 0; }
-                if (top < 0) { top = 0; }
-                Canvas.SetLeft(canvas, left);
-                Canvas.SetTop(canvas, top);
-
-                foreach (var item in canvas.Children.OfType<FrameworkElement>())
-                {
-                    Canvas.SetLeft(item, Canvas.GetLeft(item) - xMin);
-                    Canvas.SetTop(item, Canvas.GetTop(item) - yMin);
-
-                }
-                canvas.Measure(size);//変化なし
-                //canvas.Arrange(new(size));
+                canvas.Measure(size);
             }
         }
         private void Button1_Click(object sender, RoutedEventArgs e)
@@ -77,11 +42,10 @@ namespace _20221223_ExCanvas
 
     }
 
-
-    /// <summary>
-    /// オートサイズのCanvas、子要素のサイズや位置の変更時にサイズ更新
-    /// 入れ子になったExCanvasもオートサイズ
-    /// </summary>
+    //Thumbの移動でマイナス座標になったのを修正したり、Canvasの座標を常に0に保ちたいときは
+    //ArrangeOverrideだと移動中も更新されるのが災いする
+    //なのでサイズ変更時のみ更新されるMeasureOverrideを使うようにした
+    //座標修正はDragCompletedで行うのがいい？
     public class ExCanvas : Canvas
     {
         public ExCanvas() : base() { }
@@ -130,13 +94,11 @@ namespace _20221223_ExCanvas
         //}
 
         protected override Size MeasureOverride(Size constraint)
-        {
-            //return base.MeasureOverride(constraint);
+        {   
             if (double.IsNaN(Width) && double.IsNaN(Height))
             {
                 base.MeasureOverride(constraint);
                 Size size = new();
-                //double xMin = double.MaxValue, yMin = double.MaxValue;
                 foreach (var item in InternalChildren.OfType<FrameworkElement>())
                 {
                     double w = GetLeft(item) + item.DesiredSize.Width;
@@ -144,8 +106,6 @@ namespace _20221223_ExCanvas
                     if (size.Width < w) size.Width = w;
                     if (size.Height < h) size.Height = h;
                 }
-                //size.Width -= xMin; size.Height -= yMin;
-                //if(Parent is ExCanvas ex) { ex.Measure(size); } 
                 return size;
             }
             else
